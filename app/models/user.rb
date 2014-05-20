a1ebf2778bd8d108306227e6b6fee735a5cdf4c6
Gurprit Multani <gmultani12@gmail.com>
2014-05-19 22:29:39 -0400

    Add pins/users association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  has_many :pins
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
